9fcb1f3d2dd7dd4b9a3349f1c375aad7d6a62dc6
Mang-Joo <wjwan0915@gmail.com>
2024-03-10 12:36:55 +0900

    panic or no panic

---
+++ src/error/panic_or_no_panic.rs
@@ -0,0 +1,23 @@
+pub struct Guess {
+    value: i32,
+}
+
+impl Guess {
+    pub fn new(value: i32) -> Self {
+        if value < 1 || value > 100 {
+            panic!("Gues value must be between 1 and 100, got {}", value);
+        }
+
+        Guess { value }
+    }
+
+    pub fn value(&self) -> i32 {
+        self.value
+    }
+}
+
+fn main() {
+    let guess = Guess::new(-1);
+
+    println!("guess is {}", guess.value);
+}

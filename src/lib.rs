d22869341e636a0e17aecd96cf897626b6e28ca7
Mang-Joo <wjwan0915@gmail.com>
2024-03-10 20:42:50 +0900

    trait

---
+++ src/lib.rs
@@ -0,0 +1,59 @@
+use std::fmt::{Debug, Display, format};
+
+pub trait Summary {
+    fn summarize(&self) -> String {
+        String::from("(Read more...)")
+    }
+
+    fn summarize_author(&self) -> String;
+}
+
+pub struct NewsArticle {
+    pub headline: String,
+    pub location: String,
+    pub author: String,
+    pub content: String,
+}
+
+impl Summary for NewsArticle {
+    fn summarize_author(&self) -> String {
+        format!("@{}", self.author)
+    }
+    // fn summarize(&self) -> String {
+    //     format!("{}, by {} ({})", self.headline, self.author, self.location)
+    // }
+}
+
+pub struct Tweet {
+    pub username: String,
+    pub content: String,
+    pub reply: bool,
+    pub retweet: bool,
+}
+
+impl Summary for Tweet {
+    fn summarize(&self) -> String {
+        format!("{}: {}", self.username, self.content)
+    }
+
+    fn summarize_author(&self) -> String {
+        format!("@{}", self.username)
+    }
+}
+
+
+pub fn notify(item: &impl Summary) {
+    println!("Breaking news! {}", item.summarize());
+}
+
+fn some_function<T, U>(t: &T, u: &U) -> i32
+    where
+        T: Display + Clone + PartialOrd,
+        U: Clone + Debug + Display + PartialOrd
+{
+    if t >= u {
+        t as i32
+    } else {
+        u as i32
+    }
+}

914147942165f5a924b9934725ca2fe89c1616f6
Mang-Joo <wjwan0915@gmail.com>
2024-03-12 22:44:25 +0900

    closer

---
+++ src/closer/closer.rs
@@ -0,0 +1,86 @@
+mod rectangle;
+
+use std::thread;
+use crate::rectangle::Rectangle;
+
+#[derive(Debug, PartialEq, Copy, Clone)]
+enum ShirtColor {
+    Blue,
+    Red,
+}
+
+struct Inventory {
+    shirts: Vec<ShirtColor>,
+}
+
+impl Inventory {
+    fn giveaway(&self, user_preference: Option<ShirtColor>) -> ShirtColor {
+        user_preference.unwrap_or_else(|| self.most_stocked())
+    }
+
+    fn most_stocked(&self) -> ShirtColor {
+        let mut num_red = 0;
+        let mut num_blue = 0;
+
+        for color in &self.shirts {
+            match color {
+                ShirtColor::Blue => { num_blue + 1; }
+                ShirtColor::Red => { num_red + 1; }
+            }
+        }
+
+        if num_red > num_blue {
+            ShirtColor::Red
+        } else {
+            ShirtColor::Blue
+        }
+    }
+}
+
+fn main() {
+    let store = Inventory {
+        shirts: vec![ShirtColor::Blue, ShirtColor::Red, ShirtColor::Blue]
+    };
+
+    let user_pref1 = Some(ShirtColor::Red);
+    let first_giveaway = store.giveaway(user_pref1);
+    println!(
+        "The user with preference {:?} gets {:?}",
+        user_pref1, first_giveaway
+    );
+
+    let user_pref2 = None;
+    let giveaway2 = store.giveaway(user_pref2);
+
+    println!(
+        "The user with preference {:?} gets {:?}",
+        user_pref2, giveaway2
+    );
+
+    let mut list = vec![1, 2, 3];
+    println!("Before defining closure: {:?}", list);
+
+    let mut borrows_mutably = || list.push(7);
+
+    borrows_mutably();
+    println!("After calling closure: {:?}", list);
+
+    let list = vec![1, 2, 3];
+    println!("Before defining closure: {:?}", list);
+
+    thread::spawn(move || println!("From thread: {:?}", list))
+        .join()
+        .unwrap();
+
+    let a = vec![1, 2, 3];
+    let g = a.iter().map(|vv| vv + 0).collect::<Vec<i32>>();
+
+    let mut list = [
+        Rectangle { width: 10, height: 1 },
+        Rectangle { width: 3, height: 5 },
+        Rectangle { width: 7, height: 12 },
+    ];
+
+    list.sort_by_key(|r| r.width);
+    println!("list is {:#?}", list);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,69 @@
-
 fn main() {
-    /* 범위 주석 */
-    let _x/*: i16*/ = 10;
-    // comment
-    println!("Hello, world!");
+    let a = Solution::roman_to_int(String::from("MCMXCIV"));
+
+    println!("{a}");
+}
+
+struct Solution;
+
+impl Solution {
+    pub fn roman_to_int(s: String) -> i32 {
+        let s_translated = s
+            .replace("IV", "IIII")
+            .replace("IX", "VIIII")
+            .replace("XL", "XXXX")
+            .replace("XC", "LXXXX")
+            .replace("CD", "CCCC")
+            .replace("CM", "DCCCC");
+
+        s_translated.chars().map(|c| {
+            match c {
+                'I' => 1,
+                'V' => 5,
+                'X' => 10,
+                'L' => 50,
+                'C' => 100,
+                'D' => 500,
+                'M' => 1000,
+                _other => 0
+            }
+        }).sum()
+    }
+}
+
+enum Roman {
+    I,
+    V,
+    X,
+    L,
+    C,
+    D,
+    M
+}
+
+impl Roman {
+    fn find_value(&self) -> i32 {
+        match &self {
+            Roman::I => {1}
+            Roman::V => {5}
+            Roman::X => {10}
+            Roman::L => {50}
+            Roman::C => {100}
+            Roman::D => {500}
+            Roman::M => {1000}
+        }
+    }
+
+    fn find_char(check_value: char) -> Self {
+        match check_value {
+            'I' => {Roman::I}
+            'V' => {Roman::V}
+            'X' => {Roman::X}
+            'L' => {Roman::L}
+            'C' => {Roman::C}
+            'D' => {Roman::D}
+            'M' => {Roman::M}
+            other => {Roman::I}
+        }
+    }
 }
